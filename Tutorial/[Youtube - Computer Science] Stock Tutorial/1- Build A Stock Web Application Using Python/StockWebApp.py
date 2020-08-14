b61ab7237cb3836678552d9e4b387cf4d70b52ad
patricklee90 <40401455+patricklee90@users.noreply.github.com>
2020-08-14 23:41:23 +0800

    updated Data and initiate Python Code

---
+++ Tutorial/[Youtube - Computer Science] Stock Tutorial/1- Build A Stock Web Application Using Python/StockWebApp.py
@@ -0,0 +1,14 @@
+# Description: This is a stock market dashboard to show some charts and data on some stock
+
+# Import the libraries
+import streamlit as st
+import pandas as pd
+from PIL import Image
+
+# Add a title and an image
+st.write("""
+# Stock Market Web Application
+Visually show data on a stock! Date range from Jan 2, 2020 - Aug 4, 2020
+""")
+
+
